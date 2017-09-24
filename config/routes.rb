56d788daa100832dbad2034be63f4c66bdf89e50
Edgar <zorro.ej@gmail.com>
2017-09-24 13:08:56 -0700

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
+
+  resources :users
 end
