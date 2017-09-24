0a8761b61fb9a25107b701247bded123b628103e
Edgar <zorro.ej@gmail.com>
2017-09-24 13:53:44 -0700

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  root "home#index"
 
   resources :users
 end
